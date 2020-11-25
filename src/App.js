4c34d2c848c9f733b1f6c98cdc29a5f6636c8f4d
SavadMKD <savadmoorkkanad@gmail.com>
2020-11-25 15:28:36 +0530

    Sorting to do completed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css';
 
 // Importing Components
@@ -7,16 +7,39 @@ import TodoList from './components/TodoList'
 
 function App() {
 
+  // State stuff
   const [ inputText, setInputText ] = useState("");
   const [ todos, setTodos ] = useState([]);
+  const [ status, setStatus ] = useState("all");
+  const [ filteredTodos, setFilteredTodos ] = useState([]);
+
+  // Use effects
+  useEffect(() => {
+    filterHandler();
+  }, [ todos, status ]);
+
+  // Functions
+  const filterHandler = () => {
+    switch(status){
+      case 'completed':
+        setFilteredTodos(todos.filter(todo => todo.completed === true));
+        break;
+      case 'uncompleted':
+        setFilteredTodos(todos.filter(todo => todo.completed === false));
+        break;
+      default:
+        setFilteredTodos(todos);
+        break;
+    }
+  };
 
   return (
     <div className="App">
        <header>
           <h1>Savad's Todo List </h1>
        </header>
-       <Form inputText = { inputText } todos = { todos } setTodos = { setTodos } setInputText = { setInputText } />
-       <TodoList setTodos = { setTodos } todos = { todos } />
+       <Form setStatus = { setStatus } inputText = { inputText } todos = { todos } setTodos = { setTodos } setInputText = { setInputText } />
+       <TodoList filteredTodos = { filteredTodos } setTodos = { setTodos } todos = { todos } />
     </div>
   );
 }
